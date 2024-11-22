e26747eacf9093652825ac3fa4b44d164c276c1b
Aditya Sharma <adityasharma220803@gmail.com>
2024-11-22 23:39:32 +0530

    intial commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Aditya Sharma on 20/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount=0.0
+    @State private var noofPeople=2
+    @State private var tipPercentage=20
+    @FocusState private var amountisFoucsed: Bool
+    let tipPercentages = [10,20,30,40,0]
+    
+    
+    var totalPerPerson: Double{
+        let peopleCount=Double(noofPeople+2)
+        let tips=Double(tipPercentage)
+        let tipvalue=checkAmount/100*tips
+        let grandTotal=checkAmount+tipvalue
+        let amountPerPerson=grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
+    
+    var body: some View {
+        NavigationStack {
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount,format:.currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountisFoucsed)
+                    Picker("Number of people",selection: $noofPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                Section("How much do you want to Tip?"){
+                    Picker("Tip Percentage",selection: $tipPercentage){
+                        ForEach(tipPercentages, id:\.self){
+                            Text($0,format:.percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section{
+                    Text(totalPerPerson,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar{
+                if amountisFoucsed{
+                    Button("Done"){
+                        amountisFoucsed = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
